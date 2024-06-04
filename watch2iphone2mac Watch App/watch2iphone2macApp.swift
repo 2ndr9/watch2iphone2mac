55bf6f351f5ffe9accc1db6c33478e635f8e5e90
2ndr9 <53454871+2ndr9@users.noreply.github.com>
2024-06-04 15:40:29 +0900

    Initial Commit

---
+++ watch2iphone2mac Watch App/watch2iphone2macApp.swift
@@ -0,0 +1,17 @@
+//
+//  watch2iphone2macApp.swift
+//  watch2iphone2mac Watch App
+//
+//  Created by RikuTsunoda on 4/6/06.
+//
+
+import SwiftUI
+
+@main
+struct watch2iphone2mac_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watch2iphone2mac/watch2iphone2macApp.swift
@@ -0,0 +1,17 @@
+//
+//  watch2iphone2macApp.swift
+//  watch2iphone2mac
+//
+//  Created by RikuTsunoda on 4/6/06.
+//
+
+import SwiftUI
+
+@main
+struct watch2iphone2macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
